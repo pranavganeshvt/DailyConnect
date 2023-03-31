c4cc78c67d12835749e23d24aae40b77ce3b3632
pranavganeshvt <Shruthi@7556>
2023-03-31 02:55:01 -0400

    First Commit - Pranav Ganesh

---
+++ app/src/main/java/com/example/dailyconnect/SecureCheckoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dailyconnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecureCheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.secure_checkout)
+    }
+}
