6638a629dca180573a8354b9fa038a127e8b145d
pranavganeshvt <Shruthi@7556>
2023-04-03 00:19:47 -0400

    Add Secure Checkout Page- Pranav Soma

--- app/src/main/java/com/example/dailyconnect/SecureCheckoutActivity.kt
+++ app/src/main/java/com/example/dailyconnect/SecureCheckoutActivity.kt
@@ -1,11 +1,21 @@
 package com.example.dailyconnect
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class SecureCheckoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.secure_checkout)
+
+        val checkout = findViewById<Button>(R.id.checkout)
+        checkout.setOnClickListener {
+            val intent1 = Intent(this@SecureCheckoutActivity, LoginPage::class.java)
+            startActivity(intent1)
+        }
     }
 }
