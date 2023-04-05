1340cc06041a623cbcd99d7597a9b161f9f173ab
nikitayeole <nkyeole@gmail.com>
2023-04-05 00:00:39 -0400

    Added the Menu items to Homepage and added the images

--- app/src/main/java/com/example/dailyconnect/HomePageActivity.kt
+++ app/src/main/java/com/example/dailyconnect/HomePageActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 
@@ -12,7 +14,7 @@ class HomePageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        val movieImage = findViewById<ImageView>(R.id.movies)
+        val movieImage = findViewById<ImageView>(R.id.movie)
         movieImage.setOnClickListener {
             val intent = Intent (this@HomePageActivity, TheatresListActivity::class.java)
             startActivity(intent)
@@ -24,6 +26,24 @@ class HomePageActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.settings, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.option_1 -> {
+                // Handle option 1 selection
+                return true
+            }
+            R.id.option_2 -> {
+                // Handle option 2 selection
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 
 }
