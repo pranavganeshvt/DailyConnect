069270e974f3f8a1b7394fe1428ebc6212deef66
nikitayeole <nkyeole@gmail.com>
2023-04-25 02:50:41 -0400

    Added the wallet page

---
+++ app/src/main/java/com/example/dailyconnect/ui/WalletActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dailyconnect.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dailyconnect.R
+
+class WalletActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallet)
+    }
+}
